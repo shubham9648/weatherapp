2513cf8d3e5ce700405e81e05b1dcd6804eb8e21
Shubham Tiwari <tshubham9648@gmail.com>
2022-04-02 00:42:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios'
+import Data from './Data';
+
 
 function App() {
+   const [city, setCity] = React.useState("")
+   const [data, setData] = React.useState({
+    description:"",
+    country:"",
+    temp:0,
+    pressure:0,
+    humidity:0,
+    feels_like:0,
+    temp_max:0,
+    temp_min:0,
+    sunrise:0,
+    sunset:0
+   })
+  const handleClick = () =>{
+ axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8b71704e3be0cf8d20142fd546b9db86`)
+  .then((res)=>{
+    setData({
+    description:res.data.weather[0],
+    country:res.data.sys.country,
+    temp:res.data.main.temp,
+    pressure:res.data.main.pressure,
+    humidity:res.data.main.humidity,
+    feels_like:res.data.main.feels_like,
+    temp_max:res.data.main.temp_max,
+    temp_min:res.data.main.temp_min,
+    sunrise:res.data.sys.sunrise,
+    sunset:res.data.sys.sunset
+    })
+  })
+ }
+   //  const finalData = await res.json()
+  //  console.log(finalData) 
+    
+    
+    React.useEffect(()=>{
+      // getData()
+    },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className='header'>
+      <h1 className='main-heading'>Weather App in ReactJS</h1>
+      <input type="text" className='form' value={city} 
+      onChange={(event)=>{
+        setCity(event.target.value)  
+      }}/>
+      <button type='submit' onClick={handleClick}>get Tempreture</button>
+      </div>
+      <Data text={data} />
+        
+      
+    </>
   );
 }
 
---
+++ src/Data.js
@@ -0,0 +1,47 @@
+import React from "react";
+import './App.css';
+const Data = ({text}) =>{
+    return (
+        <>
+        <section className="main-container">
+      <div className='child-container'>
+          <h1 className="des">Country</h1>
+          <h2 className="two">{text.country}</h2>
+        </div>
+        
+         <div className='child-container'>
+          <h1 className="des">Current Tempreture</h1>
+          <h2 className="two">{text.temp}</h2>
+        </div>
+        <div className='child-container'>
+          <h1 className="des">Feels Like</h1>
+          <h2 className="two">{text.feels_like}</h2>
+        </div>
+        <div className='child-container'>
+          <h1 className="des">Minimum Tempreture</h1>
+          <h2 className="two">{text.temp_min}</h2>
+        </div>
+        <div className='child-container'>
+          <h1 className="des">Maximum Tempreture</h1>
+          <h2 className="two">{text.temp_max}</h2>
+        </div>
+        <div className='child-container'>
+          <h1 className="des">Current Humidity</h1>
+          <h2 className="two">{text.humidity}</h2>
+        </div>
+       
+        <div className='child-container'>
+          <h1 className="des">Sunrise</h1>
+          <h2 className="two">{text.sunrise}</h2>
+        </div>
+        <div className='child-container'>
+          <h1 className="des">Sunset</h1>
+          <h2 className="two">{text.sunset}</h2>
+        </div> 
+      
+    </section>
+        </>
+    )
+}
+
+export default Data
